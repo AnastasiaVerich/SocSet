71edc269ebb03b3706d2e3cd699982abc899893d
AnastasiaVerich <a.verich.m@gmail.com>
2021-07-26 19:33:38 +0300

    Updates

--- src/UI/component/Profile/ProfileInfo/any/Contact.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const Contact = (props: any) => {
-    return (
-        <div><b>{props.contactTitle}: </b>{props.contactValue}</div>
-    )
-}
--- src/UI/utils/validators/validators.tsx
+++
@@ -1,8 +0,0 @@
-export const requiredField=(value:any)=>{
-    if(value) return undefined
-    return "Field is required"
-}
-export const maxLength=(maxLength: number)=>(value:any)=>{
-    if(value && value.length>maxLength) return `max lenght is ${maxLength} symbols`
-    return undefined
-}
